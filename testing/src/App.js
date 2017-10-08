2b1380c3064549924645d23ce9ad9ba11776e485
Sairam Singireesu <sairamsingireesu@gmail.com>
2017-10-09 04:02:26 +0530

    penultimate commit.awaiting hover changes

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,136 +1,181 @@
 import React, { Component } from "react";
+import $ from "jquery";
 import Chart from "chart.js";
 import logo from "./logo.svg";
 import "./App.css";
 import ServerHandler from "./ServerHandler";
+import SelectField from "material-ui/SelectField";
+import MenuItem from "material-ui/MenuItem";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      option: "temperature0",
-      scale: "scale"
+      selected: ["temperature0"],
+      scale: "recent",
+      colorSet: [
+        "green",
+        "orange",
+        "black",
+        "blue",
+        "yellow",
+        "brown",
+        "violet",
+        "indigo",
+        "purple",
+        "magenta"
+      ]
     };
     ServerHandler(this);
+    this.emptyChart();
   }
-
+  emptyChart(){
+      Chart.plugins.register({
+  afterDraw: function(chart) {
+    if (chart.data.datasets.length === 0) {
+      // No data is present
+      var ctx = chart.chart.ctx;
+      var width = chart.chart.width;
+      var height = chart.chart.height
+      chart.clear();
+      
+      ctx.save();
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.font = "16px normal 'Helvetica Nueue'";
+      ctx.fillText('No data to display', width / 2, height / 2);
+      ctx.restore();
+    }
+  }
+});
+  }
   componentDidMount() {
+    // console.log(this.state);
+    // this.paintChart(this.state);
     var ctx = document.getElementById("myChart").getContext("2d");
-    // var sensorVals = this.state['temperature0']['recent'];
-    // console.log('in main app'+ sensorVals);
-
     var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: "line",
-
-      // The data for our dataset
-      // var sensorVals = this.state['temperature0'];
-      // var xaxis =  this.state['times'];
-      // xaxis.map((val))
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
-        datasets: [
-          {
-            label: "My First dataset",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [0, 10, 5, 2, 20, 30, 45]
-          }
-        ]
-      },
-
-      // Configuration options go here
-      options: {}
+      type: "line"
     });
   }
   componentWillUpdate(nextProps, nextState) {
+    this.paintChart(nextState);
+  }
+
+  paintChart(state) {
     var ctx = document.getElementById("myChart").getContext("2d");
-    var currentSensor = this.state.option;
-    var scale = this.state.scale;
+    var currentSensors = state.selected;
+    var scale = state.scale;
+    var multiId = 0;
     // console.log("current sensor:" + currentSensor);
-    var sensorVals = nextState[currentSensor];
+    var sensorVals = currentSensors.map(val => state[val]);
+    // console.log('filtered sensors'+sensorVals);
+    function pointColorSet(a){
+      // console.log('change color'+a);
+      var data = a[scale];
+      var {min,max} = a; 
+      console.log('min'+min,'max'+max);
+      return data.map(val=>(val.val<min)||(val.val>max)?'red':'green');
+    }
+    var data = sensorVals
+      ? sensorVals.map(sensorData => {
+          return {
+            label: currentSensors[multiId],
+            // backgroundColor: "rgb(255,255, 255)",
+            borderColor: this.state.colorSet[multiId],
+            pointBackgroundColor: pointColorSet(sensorVals[multiId]),
+            pointStyle : 'rect',
+            radius: 5,
+            // borderWidth: "0.8px",
+            data: sensorVals[multiId++][scale].map(val => val.val)
+            }
+          })
+      : [];
+    var labels = sensorVals[0] ? sensorVals[0][scale].map(val => val.key) : [];
+    // console.log('lables'+labels);
     // var recentVals = sensorVals['recent'];
-    // for(var key in sensorVals){
-    //   console.log('sensorVals property'+key);
-    // }
     // var minuteVals = sensorVals['minute'];
     // console.log('in main app'+ sensorVals['recent']);
 
     var chart = new Chart(ctx, {
       // The type of chart we want to create
       type: "line",
-
       // The data for our dataset
       // var sensorVals = this.state['temperature0'];
       // var xaxis =  this.state['times'];
       // xaxis.map((val))
       data: {
-        labels:
-          sensorVals[scale] !== undefined
-            ? sensorVals[scale].map(val => val.key)
-            : [],
-        datasets: [
-          {
-            label: currentSensor,
-            backgroundColor: "rgb(255,255, 255)",
-            borderColor: "green",
-            data:
-              sensorVals[scale] !== undefined
-                ? sensorVals[scale].map(val => val.val)
-                : []
-          }
-        ]
+        labels: labels,
+        datasets: data,
       },
-
-      // Configuration options go here
-      options: {
-        // showLines:false,
-        animation: {
-          duration: 0
+        // Configuration options go here
+        options: {
+          title: {
+            display: true,
+            text: 'Real-time Sensor Data',
+            fontsize:'40px'
+          },
+          elements: {
+            line: {
+              tension: 0 // disables bezier curves
+            }
+          },
+          animation: {
+            duration: 0
+          },
         }
-      }
     });
+    // console.log('chart'+Object.keys(chart));
+    
   }
-  onOptionChange(e) {
-    console.log("option change to " + e.target.value);
-    this.setState({ option: e.target.value });
+  onOptionChange(e, index, vals) {
+    // console.log("option change to " + vals);
+    this.setState({ selected: vals });
   }
   onScaleChange(e) {
     this.setState({
       scale: e.target.value
     });
   }
+
   render() {
-    var options = [];
+    var optList = [];
+    var selected = this.state.selected;
     for (var key in this.state) {
-      if (key !== "sensor" && key !== "option")
-        options.push(
-          <option key={key} value={key}>
-            {key}
-          </option>
+      if (
+        key !== "sensor" &&
+        key !== "selected" &&
+        key !== "scale" &&
+        key !== "colorSet"
+      )
+        optList.push(
+          <MenuItem
+            key={key}
+            insetChildren={true}
+            checked={selected && selected.indexOf(key) > -1}
+            value={key}
+            primaryText={key}
+          />
         );
+      // console.log('option jsx'+optList);
     }
     // var options = this.state.map();
 
     return (
       <div>
-        <div style={{ width: "550px", height: "300px" }}>
-          <canvas id="myChart" width="20" height="20" />
-        </div>
-        <div style={{ float: "right" }}>
-          <select
-            onChange={this.onOptionChange.bind(this)}
-            value={this.state.option}
-          >
-            {options}
-          </select>
+          
+      <canvas id="myChart" width='800px' height='300px' />
+        
+        <div style={{ float: "left" }}>
+          <MuiThemeProvider>
+          <SelectField
+              multiple={true}
+              hintText="Select a Sensor"
+              value={selected}
+              onChange={this.onOptionChange.bind(this)}
+            >
+              {optList}
+            </SelectField>
+          </MuiThemeProvider>
           <div>
             <form>
               <input
--- testing/src/ServerHandler.js
+++ testing/src/ServerHandler.js
@@ -1,72 +1,85 @@
 import openSocket from "socket.io-client";
-import $ from 'jquery';
+import $ from "jquery";
 // var tempSocket = openSocket('http://interview.optumsoft.com');
-var tempSocket = openSocket('http://interview.optumsoft.com');
-console.log('openSocket');
-const ServerHandler = (context)=>{
-tempSocket.on('connection',(data)=>{
-  console.log('connected');
-});
-   var sensorNames;
-   var sensorConfig;
-  $.get(`http://interview.optumsoft.com/sensornames`,function(data){
+var tempSocket = openSocket("http://interview.optumsoft.com");
+console.log("openSocket");
+const ServerHandler = context => {
+  tempSocket.on("connection", data => {
+    console.log("connected");
+  });
+  var sensorNames;
+  var sensorConfig;
+  var index = 0;
+  $.get(`http://interview.optumsoft.com/sensornames`, function(data) {
+    // console.log(data);
+    sensorNames = data;
+    $.get(`http://interview.optumsoft.com/config`, function(data) {
       // console.log(data);
-      sensorNames = data;
-      $.get(`http://interview.optumsoft.com/config`,function(data){
-      // console.log(data);
-      sensorConfig=data;
-      console.log('sensor details'+sensorNames,sensorConfig);
-      sensorNames.map(name=>{
-        // var 
-        context.setState({[name]:{
-          'recent':[],
-          'minute':[],
-          'min': sensorConfig[name].min,
-          'max':sensorConfig[name].max,
-        }});
+      sensorConfig = data;
+      // console.log('sensor details'+sensorNames,sensorConfig);
+      sensorNames.map(name => {
+        // var
+        console.log('sensorConfig'+sensorConfig[name]);
+        context.setState({
+          [name]: {
+            recent: [],
+            minute: [],
+            min: sensorConfig[name].min,
+            max: sensorConfig[name].max
+          }
+        });
       });
-      sensorNames.map(name=>tempSocket.emit('subscribe',name));
-    
+      sensorNames.map(name => tempSocket.emit("subscribe", name));
+
       // sensorConfig.map()
     });
-      
-    });
-    
-  
+  });
+
   // console.log('current context'+context.state['temperature0']);
   // console.log('sensor details'+context.state.sensorNames,context.sensorConfig);
-  tempSocket.on('data',function({key,val,sensor,type,scale}){
+  // tempSocket.on("data", function(data) {
+  //   if (data.type === "init") console.log("inti", data);
+  // });
+  tempSocket.on("data", function(data) {
     // console.log('data'+key,val,sensor);
     // console.log('currnet state'+context.state.name);
-    if(type !== 'init')
-    {
-    var sensorVals = context.state[sensor];
-    // console.log('sensorVals found'+sensorVals);
-    // console.log('sensor values for scale '+scale,sensorVals[scale]);
-    var scaleVals = sensorVals[scale];
-    // console.log('before change');
-    // scaleVals.map(val=>console.log(val.key,val.val));
-    // console.log('allvalues:'+scaleVals);
-    if(type === 'update')
-    {
-      scaleVals.push({
-        key,
-        val,
+    if (data.type === "init") {
+      var { recent, minute } = data;
+      var {min,max} = context.state["temperature" + index];
+      // console.log('for init,'+sensorVals[min],sensorVals[max]);
+      context.setState({
+        ["temperature" + index]: {
+          recent: recent,
+          minute: minute,
+          min,
+          max
+        }
       });
-  }
-  else if (type === 'delete'){
-      scaleVals=scaleVals.filter((history)=>(history.key !== key));
-      // console.log('deleted from '+sensorVals[scale]);
-  }
-  sensorVals[scale]= scaleVals;
-  // console.log('after updation');
-      // sensorVals[scale].map(val=>console.log(val.key,val.val));
-  // console.log('after updation..all the values'+sensorVals[scale]);
-    context.setState({
-        sensor:sensorVals,
+      index++;
+    } else {
+      var { key, val, sensor, type, scale } = data;
+      var sensorVals = context.state[sensor];
+      console.log('before updation..all the values'+sensorVals,sensorVals[max]);
+      // console.log('sensor values for scale '+scale,sensorVals[scale]);
+      var scaleVals = sensorVals[scale];
+      // console.log('before change');
+      // scaleVals.map(val=>console.log(val.key,val.val));
+      if (type === "update") {
+        scaleVals.push({
+          key,
+          val
+        });
+      } else if (type === "delete") {
+        scaleVals = scaleVals.filter(history => history.key !== key);
+        // console.log('deleted from '+sensorVals[scale]);
+      }
+      sensorVals[scale] = scaleVals;
+      console.log('after updation..all the values'+sensorVals[min],sensorVals[max]);
+      context.setState({
+        [sensor]: sensorVals
       });
     }
   });
-}
+};
 
 export default ServerHandler;
